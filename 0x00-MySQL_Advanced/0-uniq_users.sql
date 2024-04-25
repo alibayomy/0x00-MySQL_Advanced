2ecbb1b529c05f15f8e9684122046d3dde14609c
alibayomy <alibayomy12@gmail.com>
2024-04-25 12:56:28 +0000

    adding space to the comment

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,4 +1,4 @@
---CREATE user table with attr (id , email , name)
+-- CREATE user table with attr (id , email , name)
 CREATE TABLE IF NOT EXISTS users (
 	id INT NOT NULL AUTO_INCREMENT ,
 	email VARCHAR(255) NOT NULL UNIQUE,
