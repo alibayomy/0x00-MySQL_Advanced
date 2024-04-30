f8f222f5bb0341f97ba4e7dd67fc6d24de9bb3f9
alibayomy <alibayomy12@gmail.com>
2024-04-30 09:30:22 -0700

    adding the new_document variable

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -8,5 +8,5 @@ def insert_school(mongo_collection, **kwargs):
     """
     inserts a new document
     """
-    mongo_collection.insert_one(kwargs)
-    return mongo_collection.inserted_id
+    new_document = mongo_collection.insert_one(kwargs)
+    return new_document.inserted_id
